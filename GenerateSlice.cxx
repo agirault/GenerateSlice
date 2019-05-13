a330f2893349d1bf1f555d37d4fc50b8bfe20530
Alexis Girault <alexis.girault@kitware.com>
2019-05-13 14:27:45 -0400

    Resize generated slice

--- GenerateSlice.cxx
+++ GenerateSlice.cxx
@@ -1,9 +1,14 @@
 #include <vtkImageData.h>
 #include <vtkIntArray.h>
 #include <vtkSmartPointer.h>
+#include <vtkImageResize.h>
 #include <vtkImageReslice.h>
 
-vtkSmartPointer<vtkImageData> GenerateSlice(vtkImageData* inputData, int axis, int slice)
+vtkSmartPointer<vtkImageData> GenerateSlice(vtkImageData* inputData,
+                                            int axis,
+                                            int slice,
+                                            int sizeX = -1,
+                                            int sizeY = -1)
 {
   // Ensure axis is valid (I,J,K)
   if (axis < 0 || axis > 2)
@@ -30,12 +35,55 @@ vtkSmartPointer<vtkImageData> GenerateSlice(vtkImageData* inputData, int axis, i
   reslice->SetInputData(inputData);
   reslice->Update();
 
-  // Use ShallowCopy to free the reslice filter
-  vtkSmartPointer<vtkImageData> output =
-    vtkSmartPointer<vtkImageData>::New();
-  output->ShallowCopy(reslice->GetOutput());
+  // Lambda to return a shallow copy of the output to free the filters
+  auto getShallowCopyOfFilterOutput = [](vtkImageAlgorithm* filter)
+  {
+    vtkSmartPointer<vtkImageData> outputCopy =
+      vtkSmartPointer<vtkImageData>::New();
+    outputCopy->ShallowCopy(filter->GetOutput());
+    return outputCopy;
+  };
 
-  return output;
+  // Return right away if no resizing necessary
+  if (sizeX < 0 || sizeY < 0)
+  {
+    return getShallowCopyOfFilterOutput(reslice);
+  }
+
+  // Check sizes
+  int iDims[3], oDims[3];
+  oDims[axis] = 1;
+  if (axis == 0)
+  {
+    oDims[1] = sizeX;
+    oDims[2] = sizeY;
+  }
+  else if (axis == 1)
+  {
+    oDims[0] = sizeX;
+    oDims[2] = sizeY;
+  }
+  else if (axis == 2)
+  {
+    oDims[0] = sizeX;
+    oDims[1] = sizeY;
+  }
+  reslice->GetOutput()->GetDimensions(iDims);
+  if (iDims[0] == oDims[0] &&
+      iDims[1] == oDims[1] &&
+      iDims[2] == oDims[2])
+  {
+    return getShallowCopyOfFilterOutput(reslice);
+  }
+
+  // Resize
+  vtkSmartPointer<vtkImageResize> resize =
+    vtkSmartPointer<vtkImageResize>::New();
+  resize->SetOutputDimensions(oDims);
+  resize->SetInputData(reslice->GetOutput());
+  resize->Update();
+
+  return getShallowCopyOfFilterOutput(resize);
 }
 
 int main(int, char *[])
@@ -46,7 +94,7 @@ int main(int, char *[])
   image->SetSpacing(0.1, 0.2, 0.15);
   image->AllocateScalars(VTK_INT,1);
 
-  vtkSmartPointer<vtkImageData> output = GenerateSlice(image, 0, 100);
+  vtkSmartPointer<vtkImageData> output = GenerateSlice(image, 2, 100, 10, 10);
   assert(output != nullptr);
   output->Print(std::cout);
 
